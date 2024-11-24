6e8d758bad7f44bfcc5bb841a4632c36f7023b44
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-11-24 10:00:27 +0600

    updated list

--- basics/list.py
+++ basics/list.py
@@ -108,20 +108,5 @@ class List:
 
         print(c)
     
-    def another_test():
-        my_list = ['Mary', 'had', 'a', 'little', 'lamb']
-        
-        def my_list(my_list):
-            del my_list[3]
-            my_list[3] = 'ram'
-        
-        
-        print(my_list(my_list))
-
-
-
-
-
     
-    
-List.hotel_booking()
+# List.hotel_booking()
