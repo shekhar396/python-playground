034e2e63fbe452f27a8dac02ad77307895cf7706
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-11-25 12:33:33 +0600

    modeules_demo

---
+++ modules/modules_demo.py
@@ -0,0 +1,21 @@
+import math
+import random
+for name in dir(math):
+    print(name)
+random.seed(0)
+x=10.51
+print(math.ceil(x))
+print(math.floor(x))
+print(math.trunc(x))
+print(random.random())
+print(random.random())
+print(random.random())
+print(random.randrange(1,5,2))
+print(random.randrange(1,5,2))
+print(random.randrange(1,5))
+print(random.randrange(1,5))
+print(random.randrange(1,5))
+print(random.randrange(1,5))
+print(random.randrange(1,5))
+print(random.randrange(1,5))
+print(random.randint(0,10))
