46c64b9c4f88930fcdb3b4e704ddaafa1cf07446
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-11-25 10:39:49 +0600

    import demo

---
+++ modules/from_demo.py
@@ -0,0 +1,16 @@
+from math import pi,sin
+pi = 3.14
+print(pi)
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print(sin(pi/2))
+
+print(sin(pi / 2))
+
+
---
+++ modules/simple_math.py
@@ -0,0 +1,10 @@
+import math
+print(math.sqrt(4))
+print(math.pi)
+
+
+def sin(x):
+    return x
+
+print(math.sin(180))
+print(sin(180))
