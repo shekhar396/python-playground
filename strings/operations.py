d2692544d1b102a7c3ad144c1c930b8beda3aa33
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-12-01 12:14:36 +0600

    string operations

--- strings/operations.py
+++ strings/operations.py
@@ -1,11 +1,35 @@
-str1='a'
-str2='b'
+# String initialization
+str1 = 'a'
+str2 = 'b'
 
-print(str1+str2)
-print(5*str1)
+# Concatenating strings
+print(str1 + str2)  # Outputs: 'ab'
 
-print(ord(str1))
-print(ord(str2))
+# Repeating a string multiple times
+print(5 * str1)  # Outputs: 'aaaaa'
 
-print(chr(97)*5)
-print(chr(98))
+# Getting the ASCII value of characters
+print(ord(str1))  # Outputs: 97 (ASCII value of 'a')
+print(ord(str2))  # Outputs: 98 (ASCII value of 'b')
+
+# Using chr to get characters from ASCII values
+print(chr(97) * 5)  # Outputs: 'aaaaa'
+print(chr(98))      # Outputs: 'b'
+
+# Working with a string
+the_string = "Hello World"
+
+# Attempting to delete a character by index (will raise an error since strings are immutable)
+# del the_string[1]  # Uncommenting this line will cause a TypeError
+
+# Iterating over the string and printing characters with a space
+for ch in the_string:
+    print(ch, end=' ')  # Outputs: 'H e l l o   W o r l d'
+print()  # For a new line
+
+# Slicing the string
+print(the_string[1:3])  # Outputs: 'el'
+
+# Membership checks in a string
+print("e" in the_string)       # Outputs: True
+print("e" not in the_string)  # Outputs: False
