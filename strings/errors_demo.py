0068409c13c444056e001c08013ebb008c0404e1
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-12-03 18:23:51 +0600

    errors

---
+++ strings/errors_demo.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+try:
+    print(first_number / second_number)
+except:
+    print("This operation cannot be done.")
+
+print("THE END.")
+    
