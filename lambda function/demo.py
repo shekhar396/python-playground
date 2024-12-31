546b5e29c30e308c5d6c52f8434c902c18e3427c
Mia Abdullah Shekhar <shkhrabdullah@gmail.com>
2024-12-31 10:35:56 +0600

    more lambda

--- lambda function/demo.py
+++ lambda function/demo.py
@@ -4,3 +4,12 @@ pwr=lambda x,y: x**y
 print(two())
 print(sqr(two()))
 print(pwr(2,3))
+
+def print_function(args,fun):
+    for x in args:
+        print(fun(x))
+
+def poly(x):
+    return 2*x**2-4*x+2
+print_function([x for x in range(-2,3)],poly)
+print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2)
